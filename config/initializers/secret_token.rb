70ebcacfad0059514dc21141f3aa4ef301f69285
Robert Hintz <rhintz42@stanford.edu>
2012-08-27 22:30:36 -0700

    Adding initial rails project to repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GardenCookbook::Application.config.secret_token = 'f2161381fbf355cbabf6a94e8e73c6d7529e51cd034ef1e108ef124c32d2392ab9e050ae0c27568b2249c7399b45090daf4cdb78403e232cc374b5717c0b9f20'
