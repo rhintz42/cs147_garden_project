1194b2b97c23590e81b3cf38b09c0ecfa74554a1
Robert Hintz <rhintz42@stanford.edu>
2012-11-08 11:36:42 -0800

    Trying to fix heroku

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,8 +3,6 @@ class WeatherController < ApplicationController
 	include MyModule
 
 	def index
-
-#		gardens = Garden.where(:id => params[:garden])
 		garden = Garden.find(params[:garden])
 		loc = garden.location
 		@weatherData = Weather.new(loc).testData
